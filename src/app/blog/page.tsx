dff819b16df84b99d52445b17442081a8f24f53a
Harikesh <sharikesh462@gmail.com>
2026-02-23 23:01:21 +0530

    Add SEO optimization: sitemap, robots.txt and improved metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -16,7 +16,7 @@ export default async function BlogPage() {
       
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map((post) => (
-          <PostCard key={post.slug} {...post} />
+          <PostCard key={post.slug} {...post} date={post.date.toISOString()} />
         ))}
       </div>
       
